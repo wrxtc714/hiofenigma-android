752f372650af31cb9a2d380e101036a622610587
William Killerud <klerud@gmail.com>
2012-03-04 22:49:30 +0100

    Rewrote the thing to have a service holding and managing the timers, and a UI communicating with it.

--- 1202 AppChallenge KitchenTimer/src/edu/killerud/kitchentimer/KlerudKitchenTimer.java
+++ 1202 AppChallenge KitchenTimer/src/edu/killerud/kitchentimer/KlerudKitchenTimer.java
@@ -3,6 +3,10 @@ package edu.killerud.kitchentimer;
 import java.util.ArrayList;
 
 import android.app.Activity;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
@@ -30,11 +34,14 @@ import com.quietlycoding.android.picker.NumberPicker;
 public class KlerudKitchenTimer extends Activity
 {
 	private LinearLayout mLlContentLayout;
-	private final ArrayList<TimerView> mTimers = new ArrayList<TimerView>();
 	private LinearLayout mLlTimePicker;
 	private static NumberPicker npHours;
 	private static NumberPicker npMinutes;
 	private static NumberPicker npSeconds;
+
+	private ArrayList<TimerView> mTimerViews;
+
+	private int mNumberOfTimers;
 
 	/** Called when the activity is first created. */
 	@Override
@@ -42,92 +49,132 @@ public class KlerudKitchenTimer extends Activity
 	{
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
-
-		Log.i("KitchenTimer", "Trolololol");
-
 		mLlContentLayout = (LinearLayout) findViewById(R.id.llContentLayout);
 		mLlTimePicker = (LinearLayout) findViewById(R.id.llTimePicker);
+		mTimerViews = new ArrayList<TimerView>();
 
 		/* Sets up three TimePicker widgets for time input */
 		setupPickers();
 		/* Sets up the add- and remove timer buttons */
 		Button bAddTimer = (Button) findViewById(R.id.bAddTimer);
 		Button bRemoveTimer = (Button) findViewById(R.id.bRemoveTimer);
+
 		bAddTimer.setOnClickListener(new OnClickListener()
 		{
 			public void onClick(View v)
 			{
-				mTimers.add(new TimerView(getApplicationContext()));
-				mLlContentLayout.addView(mTimers.get(mTimers.size() - 1)
-						.getmLlTimerLayout(),
-						android.view.ViewGroup.LayoutParams.FILL_PARENT,
-						android.view.ViewGroup.LayoutParams.WRAP_CONTENT);
-				mLlContentLayout.setPadding(4, 4, 4, 4);
+				addTimer();
 			}
+
 		});
 		bRemoveTimer.setOnClickListener(new OnClickListener()
 		{
 			public void onClick(View v)
 			{
-				if (mTimers.size() <= 0)
-				{
-					return;
-				}
-				mLlContentLayout.removeView(mTimers.get(mTimers.size() - 1)
-						.getmLlTimerLayout());
-				/*
-				 * Calls the timer's remove() to stop the alarm and the
-				 * countdown, as well as free up resources.
-				 */
-				mTimers.get(mTimers.size() - 1).remove();
-				mTimers.remove(mTimers.size() - 1);
+				removeTimer();
 			}
+
 		});
 
-		try
-		{
+	}
 
-			for (int i = 0; i < savedInstanceState.size(); i++)
+	@Override
+	public void onResume()
+	{
+		super.onResume();
+		registerBroadcastReceiver();
+
+		Intent getTimers = new Intent(getApplicationContext(),
+				edu.killerud.kitchentimer.CountdownService.class);
+		getTimers.setAction("GET_TIMERS");
+		startService(getTimers);
+	}
+
+	@Override
+	public void onPause()
+	{
+		super.onPause();
+		unregisterReceiver(broadcastReceiver);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu)
+	{
+		MenuInflater inflater = getMenuInflater();
+		inflater.inflate(R.menu.menu, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item)
+	{
+		switch (item.getItemId())
+		{
+		case R.id.stopAll:
+			Intent stopTimer;
+			for (int i = 0; i < mTimerViews.size(); i++)
 			{
-				Log.i("KitchenTimer", "Trying to restore timer " + i);
-				try
-				{
-					if (savedInstanceState.getParcelable("timer" + i)
-							.toString().equals("TimerView"))
-					{
-						Log.i("KitchenTimer", "Actually restoring timer " + i);
-						mTimers.add((TimerView) savedInstanceState
-								.getParcelable("timer" + i));
-						mLlContentLayout.addView(mTimers
-								.get(mTimers.size() - 1).getmLlTimerLayout());
-					}
-				} catch (NullPointerException npe)
-				{
-					Log.i("KitchenTimer", "Added " + (i + 1)
-							+ " timers from state");
-					break;
-				}
+				stopTimer = new Intent(getApplicationContext(),
+						edu.killerud.kitchentimer.CountdownService.class);
+				stopTimer.setAction("STOP_TIMER");
+				stopTimer.putExtra("TIMER_ID", i);
+				startService(stopTimer);
 			}
-
-		} catch (NullPointerException npe)
-		{
-			Log.i("KitchenTimer", "NPE on state, defaulting");
-
-			/*
-			 * Adds a few timers so the user has something to look at right off
-			 * the bat.
-			 */
-			mTimers.add(new TimerView(getApplicationContext()));
-			mLlContentLayout.addView(mTimers.get(mTimers.size() - 1)
-					.getmLlTimerLayout());
-			mTimers.add(new TimerView(getApplicationContext()));
-			mLlContentLayout.addView(mTimers.get(mTimers.size() - 1)
-					.getmLlTimerLayout());
-			mTimers.add(new TimerView(getApplicationContext()));
-			mLlContentLayout.addView(mTimers.get(mTimers.size() - 1)
-					.getmLlTimerLayout());
-
+			return true;
+		default:
+			return super.onOptionsItemSelected(item);
 		}
+	}
+
+	protected void registerBroadcastReceiver()
+	{
+		IntentFilter ifilter = new IntentFilter();
+		ifilter.addAction("TIMER_ADDED");
+		ifilter.addAction("TIMER_REMOVED");
+		ifilter.addAction("TIMER_STOPPED");
+		ifilter.addAction("TIMER_ALARM_STOPPED");
+		ifilter.addAction("TIMER_STARTED");
+		ifilter.addAction("NUMBER_OF_TIMERS");
+		ifilter.addAction("TIMER_TICK");
+		ifilter.addAction("ALARM_SOUNDING");
+		registerReceiver(broadcastReceiver, new IntentFilter(ifilter));
+	}
+
+	protected void addTimer()
+	{
+		Intent addTimer = new Intent(getApplicationContext(),
+				edu.killerud.kitchentimer.CountdownService.class);
+		addTimer.setAction("ADD_TIMER");
+		addTimer.putExtra("TIMER_ID", mNumberOfTimers);
+
+		startService(addTimer);
+	}
+
+	protected void addTimerView()
+	{
+		mTimerViews.add(new TimerView(getApplicationContext(), mTimerViews
+				.size()));
+		mLlContentLayout.addView(mTimerViews.get(mTimerViews.size() - 1)
+				.getTimerLayout());
+	}
+
+	protected void removeTimer()
+	{
+		if (mTimerViews.size() > 0)
+		{
+			Intent removeTimer = new Intent(getApplicationContext(),
+					edu.killerud.kitchentimer.CountdownService.class);
+			removeTimer.setAction("REMOVE_TIMER");
+			removeTimer.putExtra("TIMER_ID", mTimerViews.size() - 1);
+			startService(removeTimer);
+		}
+	}
+
+	protected void removeTimerView()
+	{
+		mLlContentLayout.removeView(mTimerViews.get(mTimerViews.size() - 1)
+				.getTimerLayout());
+		mTimerViews.remove(mTimerViews.size() - 1);
 	}
 
 	/* Sets up the TimePicker widgets */
@@ -154,62 +201,84 @@ public class KlerudKitchenTimer extends Activity
 		mLlTimePicker.addView(npSeconds);
 	}
 
-	@Override
-	public void onSaveInstanceState(Bundle bundle)
+	private final BroadcastReceiver broadcastReceiver = new BroadcastReceiver()
 	{
-		super.onSaveInstanceState(bundle);
-		for (int i = 0; i < mTimers.size(); i++)
-		{
-			bundle.putParcelable("timer" + i, mTimers.get(i));
-			Log.i("KitchenTimer", "Saving timer " + i);
-		}
-	}
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu)
-	{
-		MenuInflater inflater = getMenuInflater();
-		inflater.inflate(R.menu.menu, menu);
-		return true;
-	}
-
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item)
-	{
-		switch (item.getItemId())
+		@Override
+		public void onReceive(Context context, Intent intent)
 		{
-		case R.id.stopAll:
-			for (int i = 0; i < mTimers.size(); i++)
+			if (intent == null)
 			{
-				mTimers.get(i).stop();
+				return;
 			}
-			return true;
-		default:
-			return super.onOptionsItemSelected(item);
+
+			if (intent.getAction().equals("TIMER_TICK"))
+			{
+				Log.i("TimerActivity", "Tick");
+				try
+				{
+					mTimerViews.get(intent.getIntExtra("TIMER_ID", -1))
+							.updateTick(intent.getLongExtra("TIME_LEFT", 0l));
+				} catch (IndexOutOfBoundsException e)
+				{
+					// Something went wrong elsewhere
+				}
+
+			} else if (intent.getAction().equals("TIMER_REMOVED"))
+			{
+				removeTimerView();
+			} else if (intent.getAction().equals("TIMER_STOPPED"))
+			{
+				try
+				{
+					mTimerViews.get(intent.getIntExtra("TIMER_ID", -1))
+							.resetUI();
+				} catch (IndexOutOfBoundsException e)
+				{
+					// Something went wrong elsewhere
+				}
+
+			} else if (intent.getAction().equals("ALARM_SOUNDING"))
+			{
+				mTimerViews.get(intent.getIntExtra("TIMER_ID", -1))
+						.setSounding();
+			} else if (intent.getAction().equals("TIMER_ALARM_STOPPED"))
+
+			{
+				try
+				{
+					mTimerViews.get(intent.getIntExtra("TIMER_ID", -1))
+							.resetUI();
+				} catch (IndexOutOfBoundsException e)
+				{
+					// Something went wrong elsewhere
+				}
+			} else if (intent.getAction().equals("TIMER_ADDED"))
+			{
+				addTimerView();
+			} else if (intent.getAction().equals("NUMBER_OF_TIMERS"))
+			{
+				for (int i = 0; i < intent.getIntExtra("NUMBER_OF_TIMERS", 0); i++)
+				{
+					addTimerView();
+				}
+			}
+
 		}
-	}
 
-	@Override
-	public void onPause()
-	{
-		super.onPause();
+	};
 
-	}
-
-	/* Used by TimerView the currently chosen number */
-	public static Integer getHours()
+	public static int getHours()
 	{
 		return npHours.getCurrent();
 	}
 
-	/* Used by TimerView the currently chosen number */
-	public static Integer getMinutes()
+	public static int getMinutes()
 	{
 		return npMinutes.getCurrent();
 	}
 
-	/* Used by TimerView the currently chosen number */
-	public static Integer getSeconds()
+	public static int getSeconds()
 	{
 		return npSeconds.getCurrent();
 	}
